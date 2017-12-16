e28febd4013dc7f9905a8752b9b57255ac61646a
Denniskamau <denniskamau3@gmail.com>
2017-12-16 23:18:05 +0300

    Code skeleton

---
+++ stack.js
@@ -0,0 +1,30 @@
+//Implementation of a stack using a string as the storage point
+
+var Stack = function(capacity){
+    this._capacity = capacity
+    this._storage = {}
+    this._count = 0
+};
+
+Stack.prototype.push = function(val){
+    
+
+    
+};
+
+Stack.prototype.pop = function(){
+
+};
+
+Stack.prototype.peek = function(){
+ 
+
+};
+
+Stack.prototype.count = function(){
+    
+}
+
+var myWeeklyMenu = new Stack();
+
+myWeeklyMenu.push('beens');
